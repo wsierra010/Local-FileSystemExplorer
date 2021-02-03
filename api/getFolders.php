367d80d2effd7463fd9fe6ce7a4c3443cfbadf89
Jan Puig <janpuigcapella@gmail.com>
2021-02-03 15:38:31 +0100

    Done Get SideBar folders #3

--- api/getFolders.php
+++ api/getFolders.php
@@ -2,35 +2,42 @@
     require ('documentClass.php');
 
     $dir = 'root';
-    $files = getFilesFromFolder($dir);
+    $files = getFilesFromFolder($dir)[0];
 
     function getFilesFromFolder($dir){
 
         $rootFiles = scandir('../' . $dir);
         $filesInFolder=[];
-
+        $size=0;
         foreach( $rootFiles as $file ){
-
             if( $file!='.' && $file!='..' ){
-                $path = $dir . '/' . $file;
-                $path_info = pathinfo( $path );
-                $created=0;
-                $modidied = filemtime ('../' . $path);
-                if( isset( $path_info['extension'] ) ){
-                    $size = filesize('../' . $path);
-                    $doc=new document($file, $path_info['extension'], $path, $size, $created, $modidied);
-                    array_push($filesInFolder, $doc);
-                }else{
-                    $size = 0;
-                    $content = getFilesFromFolder($path);
-                    $doc=new folder($file, 'folder', $path, $content, $size, $created, $modidied);
-                    array_push($filesInFolder, $doc);
-                }
+                $doc=createFileObject($dir, $file);
+                $size+=$doc->size;
+                array_push($filesInFolder, $doc);
             }
         }
+        return [$filesInFolder, $size];
 
-        return $filesInFolder;
+    }
 
+    function createFileObject($dir, $file){
+
+        $path = $dir . '/' . $file;
+        $path_info = pathinfo( $path );
+        $created= filectime ('../' . $path);
+        $modidied = filemtime ('../' . $path);
+        $doc=null;
+        $size=0;
+        if( isset( $path_info['extension'] ) ){
+            $size = filesize('../' . $path);
+            $doc=new document($file, $path_info['extension'], $path, $size, $created, $modidied);
+        }else{
+            $files=getFilesFromFolder($path);
+            $size = $files[1];
+            $content = $files[0];
+            $doc=new folder($file, 'folder', $path, $content, $size, $created, $modidied);
+        }
+        return $doc;
     }
 
     $myJSON = json_encode( $files );
